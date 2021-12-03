f063db9cd14598c47dd2754338ab2d501a4b1ba7
Julien Terrier <julien.terrier@orange.com>
2021-12-03 17:57:03 +0100

    [2021][day02] Part 2, first draft

--- src/main/kotlin/v2021/Day02.kt
+++ src/main/kotlin/v2021/Day02.kt
@@ -13,27 +13,54 @@ fun main() {
     )
 
     println(finalPosition.horizontal * finalPosition.depth)
+
+    val finalState = getFinalState(
+        initialState = State(position = Position(horizontal = 0, depth = 0), aim = 0),
+        steps = input.toListOfSteps()
+    )
+
+    with (finalState.position) { println(horizontal * depth) }
 }
 
 sealed interface Step {
     val amount: Int
-    fun moveFrom(startingPosition: Position) : Position
+    fun moveFrom(startingPosition: Position): Position
+    fun nextFrom(startingState: State): State
 
     data class Forward(override val amount: Int) : Step {
         override fun moveFrom(startingPosition: Position) =
             startingPosition.copy(horizontal = startingPosition.horizontal + amount)
 
+        override fun nextFrom(startingState: State): State =
+            startingState.copy(
+                position = startingState.position.copy(
+                    horizontal = startingState.position.horizontal + amount,
+                    depth = startingState.position.depth + startingState.aim * amount
+                )
+            )
     }
+
     data class Up(override val amount: Int) : Step {
         override fun moveFrom(startingPosition: Position) =
             startingPosition.copy(depth = startingPosition.depth - amount)
+
+        override fun nextFrom(startingState: State): State =
+            startingState.copy(aim = startingState.aim - amount)
     }
 
     data class Down(override val amount: Int) : Step {
         override fun moveFrom(startingPosition: Position) =
             startingPosition.copy(depth = startingPosition.depth + amount)
+
+        override fun nextFrom(startingState: State): State =
+            startingState.copy(aim = startingState.aim + amount)
     }
 }
+
+data class State(
+    val position: Position,
+    val aim: Int
+)
 
 data class Position(
     val horizontal: Int,
@@ -59,4 +86,12 @@ fun getFinalPosition(
 ): Position =
     steps.fold(initial = initialPosition) { previousPosition, step ->
         step.moveFrom(previousPosition)
+    }
+
+fun getFinalState(
+    initialState: State,
+    steps: List<Step>
+): State =
+    steps.fold(initial = initialState) { previousState, step ->
+        step.nextFrom(previousState)
     }
--- src/test/kotlin/v2021/Day02Test.kt
+++ src/test/kotlin/v2021/Day02Test.kt
@@ -1,10 +1,12 @@
 package v2021
 
 import gg.jte.aoc.v2021.Position
+import gg.jte.aoc.v2021.State
 import gg.jte.aoc.v2021.Step.Down
 import gg.jte.aoc.v2021.Step.Forward
 import gg.jte.aoc.v2021.Step.Up
 import gg.jte.aoc.v2021.getFinalPosition
+import gg.jte.aoc.v2021.getFinalState
 import gg.jte.aoc.v2021.toListOfSteps
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
@@ -45,6 +47,22 @@ internal class Day02Test {
 
                 )
         ) shouldBe Position(horizontal = 15, depth = 10)
+    }
+
+    @Test
+    fun `should move according to steps with aim`() {
+        getFinalState(
+            initialState = State(position = Position(horizontal = 0, depth = 0), aim = 0),
+            steps = listOf(
+                Forward(5),
+                Down(5),
+                Forward(8),
+                Up(3),
+                Down(8),
+                Forward(2),
+
+                )
+        ).position shouldBe Position(horizontal = 15, depth = 60)
     }
 
 }
