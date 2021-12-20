546592e69f915ca50d4eaff1c9f4f56f593ed1c5
Julien Terrier <julien.terrier@orange.com>
2021-12-20 18:03:16 +0100

    [2021][day06] Part 2

--- src/main/kotlin/v2021/Day06.kt
+++ src/main/kotlin/v2021/Day06.kt
@@ -4,44 +4,51 @@ import gg.jte.aoc.getTextFromFile
 import gg.jte.aoc.measureTimeAndPrint
 
 fun main() {
-    val input = getTextFromFile("v2021/day_06.txt").split(",").map { Lanternfish(it.toInt()) }
+    val input = getTextFromFile("v2021/day_06.txt")
+        .split(",")
+        .map { it.toInt() }
+        .toFishCensus()
 
-    measureTimeAndPrint { simulateFish(initialList = input, numberOfDays = 80).size }
+    measureTimeAndPrint { simulate(initialCensus = input, numberOfDays = 80) }
+    measureTimeAndPrint { simulate(initialCensus = input, numberOfDays = 256) }
 }
 
-fun List<Int>.toFishList() =
-    map { Lanternfish(it) }
+fun List<Int>.toFishCensus(): Map<LanternfishKind, Long> =
+    groupingBy { it }
+        .eachCount()
+        .map { LanternfishKind(it.key) to it.value.toLong()}
+        .toMap()
 
-fun simulateFish(
-    initialList: List<Lanternfish>,
+
+fun Map<LanternfishKind, Long>.countFishWithCounter(counter: Int) =
+    this[LanternfishKind(counter)] ?: 0
+
+fun simulate(
+    initialCensus: Map<LanternfishKind, Long>,
     numberOfDays: Int
-): List<Lanternfish> {
-    val sequence = generateSequence(initialList) { currentListOfFish ->
-        val numberOfFishThatWillReset = currentListOfFish.count { it.willReset() }
-
-        currentListOfFish.map { it.age() } + List(numberOfFishThatWillReset) { Lanternfish.spawn() }
+): Long {
+    val sequence = generateSequence(initialCensus) { currentState ->
+        mapOf(
+            LanternfishKind(0) to currentState.countFishWithCounter(1),
+            LanternfishKind(1) to currentState.countFishWithCounter(2),
+            LanternfishKind(2) to currentState.countFishWithCounter(3),
+            LanternfishKind(3) to currentState.countFishWithCounter(4),
+            LanternfishKind(4) to currentState.countFishWithCounter(5),
+            LanternfishKind(5) to currentState.countFishWithCounter(6),
+            LanternfishKind(6) to currentState.countFishWithCounter(7) + currentState.countFishWithCounter(0),
+            LanternfishKind(7) to currentState.countFishWithCounter(8),
+            LanternfishKind(8) to + currentState.countFishWithCounter(0),
+        )
     }
 
-    return sequence.elementAt(numberOfDays).toList()
-
+    return sequence.elementAt(numberOfDays).toList().sumOf { it.second }
 }
 
 @JvmInline
-value class Lanternfish(
+value class LanternfishKind(
     private val counter: Int
 ) {
-    fun age() = when (counter) {
-        0 -> Lanternfish(6)
-        else -> Lanternfish(counter - 1)
-    }
-
-    fun willReset() = counter == 0
-
     override fun toString(): String {
         return counter.toString()
-    }
-
-    companion object {
-        fun spawn() = Lanternfish(8)
     }
 }
--- src/test/kotlin/v2021/Day06Test.kt
+++ src/test/kotlin/v2021/Day06Test.kt
@@ -1,18 +1,18 @@
 package v2021
 
-import gg.jte.aoc.v2021.simulateFish
-import gg.jte.aoc.v2021.toFishList
-import io.kotest.matchers.collections.shouldHaveSize
+import gg.jte.aoc.v2021.simulate
+import gg.jte.aoc.v2021.toFishCensus
+import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
 internal class Day06Test {
 
     @Test
     fun `should evolve fish`() {
-        val initialFishList = listOf(3, 4, 3, 1, 2).toFishList()
-
-        simulateFish(initialList = initialFishList, numberOfDays = 18) shouldHaveSize 26
-        simulateFish(initialList = initialFishList, numberOfDays = 80) shouldHaveSize 5934
+        val fishCensus = listOf(3, 4, 3, 1, 2).toFishCensus()
+        simulate(initialCensus = fishCensus, numberOfDays = 18) shouldBe  26
+        simulate(initialCensus = fishCensus, numberOfDays = 80) shouldBe  5934
+        simulate(initialCensus = fishCensus, numberOfDays = 256) shouldBe 26984457539
     }
 
 }
