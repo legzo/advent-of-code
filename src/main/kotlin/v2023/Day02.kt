20e89bbc6c5896a3b6ba8e26e2a124a21ab69aa4
Julien Terrier <julien.terrier@orange.com>
2023-12-04 13:38:03 +0100

    [2023][day02] Part 1

---
+++ src/main/kotlin/v2023/Day02.kt
@@ -0,0 +1,50 @@
+package gg.jte.aoc.v2023
+
+import gg.jte.aoc.getTextFromFile
+import gg.jte.aoc.measureTimeAndPrint
+import gg.jte.aoc.parseWithRegex
+
+fun main() {
+    val input = getTextFromFile("v2023/day02.txt")
+    measureTimeAndPrint { input.parseAsGames().filter { it.isPossible() }.sumOf { it.id } }
+}
+
+data class Game(
+    val id: Int,
+    val draws: List<Draw>
+)
+
+data class Draw(
+    val red: Int = 0,
+    val green: Int = 0,
+    val blue: Int = 0,
+)
+
+fun String.parseAsGames(): List<Game> =
+    lines().mapNotNull {
+        it.parseWithRegex("Game ([0-9]+): (.*)") { (gameId, drawsAsString) ->
+
+            val draws = drawsAsString.split(";").map { drawAsString ->
+                Draw(
+                    red = drawAsString.findColor("red"),
+                    green = drawAsString.findColor("green"),
+                    blue = drawAsString.findColor("blue"),
+                )
+            }
+
+            Game(id = gameId.toInt(), draws = draws)
+        }
+    }
+
+
+private fun String.findColor(colorAsString: String): Int =
+    split(",")
+        .firstOrNull { it.contains(colorAsString) }
+        ?.filter { it.isDigit() }
+        ?.toInt()
+        ?: 0
+
+fun Game.isPossible(): Boolean =
+    draws
+        .all { it.red <= 12 && it.green <= 13 && it.blue <= 14 }
+
---
+++ src/test/kotlin/v2023/Day02Test.kt
@@ -0,0 +1,49 @@
+package v2023
+
+import gg.jte.aoc.v2023.Draw
+import gg.jte.aoc.v2023.Game
+import gg.jte.aoc.v2023.isPossible
+import gg.jte.aoc.v2023.parseAsGames
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class Day02Test {
+
+    private val input = """
+        Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+        Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+        Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+        Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+        """.trimIndent()
+
+    @Test
+    fun `parse game line`() {
+        input.parseAsGames() shouldHaveSize 5
+        input.parseAsGames().first() shouldBe Game(
+            id = 1,
+            draws = listOf(
+                Draw(
+                    red = 4,
+                    blue = 3,
+                ),
+                Draw(
+                    red = 1,
+                    green = 2,
+                    blue = 6,
+                ),
+                Draw(
+                    green = 2,
+                ),
+            )
+        )
+    }
+
+    @Test
+    fun `a game is possible`() {
+        input.parseAsGames()
+            .filter { it.isPossible() } shouldHaveSize 3
+    }
+
+}
